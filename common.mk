0a15b82a6a659f6f39e471642257ac09d3ae180b
Mashopy <eliasgheeraert@gmail.com>
2023-01-09 20:12:36 +0100

    sm8250-common: overlay: Match WifiResTarget with RMX3370_11.F.01
    Change-Id: I5f5b75be0c4e77dd45d3e3c052da154078e706d2

--- common.mk
+++ common.mk
@@ -354,7 +354,7 @@ PRODUCT_PACKAGES += \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
     OPlusTetheringResCommon \
-    OPlusWifiResCommon
+    WifiResTarget
 
 # Partition
 PRODUCT_BUILD_SUPER_PARTITION := false
